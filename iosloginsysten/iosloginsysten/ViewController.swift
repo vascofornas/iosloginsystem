92be945d488ef3c83327bc8d7d6514b2a043a8b5
mvasco <modestovasco@gmail.com>
2016-11-23 19:04:48 -0700

    sincronizar

---
+++ iosloginsysten/iosloginsysten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosloginsysten
+//
+//  Created by MODESTO VASCO FORNAS on 10/2/16.
+//  Copyright © 2016 Modesto Vasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
